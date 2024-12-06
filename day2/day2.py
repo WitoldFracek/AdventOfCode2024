104134fb14cc277df1adce7846f24c15260225d6
WitoldFracek <witek.fracek@gmail.com>
2024-12-06 11:36:45 +0100

    handled all cases

--- day2/day2.py
+++ day2/day2.py
@@ -16,25 +16,42 @@ def is_correct_seq(seq: QList[int]) -> bool:
 
 
 def is_correct_with_removal(seq: QList[int]) -> bool:
-    fix_batches = (
-        seq
-        .window(3)
-        .map(is_correct_seq)
-        .batch_by(lambda x: x)
-        .collect()
-    )
-    if fix_batches.len() == 1 and fix_batches[0][0]:
+    fix_batches = seq.window(3).batch_by(is_correct_seq).collect()
+    # if all windows are correct then the whole sequence is correct
+    if fix_batches.len() == 1 and is_correct_seq(fix_batches[0][0]):
         return True
+
+    # if there are 2 groups the error was at the beginning or at the end
     if fix_batches.len() == 2:
+        # left = beginning, right = end
         left, right = fix_batches
-        if not left[0] and left.len() == 1:
-            return True
-        if not right[0] and right.len() == 1:
-            return True
+        if not is_correct_seq(left[0]):
+            if left.len() > 2:
+                return False
+            if left.len() == 1:
+                return True
+            l, r = left
+            return is_correct_seq(r) or is_correct_seq(QList([l[0], r[1], r[2]])) or is_correct_seq(QList([l[0], l[1], r[2]]))
+        else:
+            if right.len() > 2:
+                return False
+            if right.len() == 1:
+                return True
+            l, r = right
+            return is_correct_seq(l) or is_correct_seq(QList([l[0], r[1], r[2]])) or is_correct_seq(QList([l[0], l[1], r[2]]))
+
+    # if there are 3 groups, only the middle one can contain errors.
+    # Otherwise, we have 2 separate errors which cannot be corrected.
     if fix_batches.len() == 3:
         _, mid, _ = fix_batches
-        if not mid[0] and mid.len() < 4:
-            return True
+        if not is_correct_seq(mid[0]) and mid.len() < 3:
+            # if the group is of length 1 it means we found a switch point of sequence monotonic (7 6 5 4 5 6 cannot be fixed)
+            if mid.len() == 1:
+                return False
+            l, r = mid
+            return is_correct_seq(QList([l[0], r[1], r[2]])) or is_correct_seq(QList([l[0], l[1], r[2]]))
+
+    # if there is more groups than 3 we have at least 2 separate errors
     return False
 
 
