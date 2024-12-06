e9090b3e1263e7b7b34c96b62b44d605016002dd
WitoldFracek <witek.fracek@gmail.com>
2024-12-06 12:45:17 +0100

    day 3 a

--- day3/day3.py
+++ day3/day3.py
@@ -1,10 +1,23 @@
 from qwlist import Lazy, QList
 from utils import read_lines
+import re
+from typing import Tuple
 
+
+def parse_tuple(instruction: str) -> Tuple[int, int]:
+    s = instruction[4:]
+    s = s[:-1]
+    l, r = s.split(',')
+    return int(l), int(r)
 
 def sol_a() -> int:
-
-    return -1
+    data = ''.join(read_lines('./input.txt'))
+    return (
+        Lazy(re.findall(r'mul\([1-9]\d{0,2},[1-9]\d{0,2}\)', data))
+        .map(parse_tuple)
+        .map(lambda pair: pair[0] * pair[1])
+        .sum()
+    )
 
 
 def sol_b() -> int:
