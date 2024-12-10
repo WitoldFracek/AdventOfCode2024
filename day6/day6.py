63901cd06d15518f2bfb73761a7046bb4bcf48f8
WitoldFracek <witek.fracek@gmail.com>
2024-12-10 17:33:23 +0100

    day 6 b attempts

--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,33 @@
 from qwlist import Lazy, QList
-from utils import read_lines
-from typing import List, Tuple, Literal
+from utils import read_lines, Option
+from typing import List, Tuple, Literal, TypeAlias, Iterable, Set, Optional
 
-def find_starting_position(board: List[List[str]]) -> Tuple[int, int] | None:
+Board: TypeAlias = QList[QList["Cell"]]
+DataA: TypeAlias = Tuple[int, int, str]
+DataB: TypeAlias = Tuple[int, int, str, Set[Tuple[int, int]]]
+
+class Cell:
+    def __init__(self, *chars):
+        self.chars = list(chars)
+
+    def __contains__(self, item: str):
+        return item in self.chars
+
+    def set(self, value: str):
+        if len(self.chars) == 1 and '' in self.chars:
+            self.chars = [value]
+        elif value not in self.chars:
+            self.chars.append(value)
+
+    def __repr__(self) -> str:
+        if len(self.chars) == 1 and '' in self.chars:
+            return '.   |'
+        return f'{"".join(sorted(self.chars)):<4}|'
+
+    def is_obstacle(self) -> bool:
+        return '#' in self.chars
+
+def find_starting_position(board: Iterable[Iterable[str]]) -> Tuple[int, int] | None:
     for i, row in enumerate(board):
         for j, c in enumerate(row):
             if c == '^':
@@ -12,70 +37,111 @@ def find_starting_position(board: List[List[str]]) -> Tuple[int, int] | None:
 def rotate(direction: str) -> str:
     return {'^': '>', '>': 'V', 'V': '<', '<': '^'}.get(direction, direction)
 
-def print_board(board: QList[QList[str]]):
-    print('\n'.join(board.map(lambda l: ''.join(l))))
+
+def deltas(direction: str) -> Tuple[int, int]:
+    return {'^': (-1, 0), 'V': (1, 0), '<': (0, -1), '>': (0, 1)}[direction]
+
+def neighbours(board: Board, x: int, y: int) -> Tuple[Option[Cell], Option[Cell], Option[Cell], Option[Cell]]:
+    return (
+        Option.wrap(board.get(x - 1, QList()).get(y)),
+        Option.wrap(board.get(x, QList()).get(y + 1)),
+        Option.wrap(board.get(x + 1, QList()).get(y)),
+        Option.wrap(board.get(x, QList()).get(y - 1))
+    )
+
+def print_board(board: Board):
+    print('\n'.join(board.map(lambda l: ''.join(l.map(repr)))))
     print()
 
-def move(board: QList[QList[str]], x: int, y: int, direction: Literal['^', '<', 'V', '>']) -> Tuple[int, int, str] | None:
-    deltas = {'^': (-1, 0), 'V': (1, 0), '<': (0, -1), '>': (0, 1)}
-    dx, dy = deltas[direction]
+def move(board: Board, x: int, y: int, direction: str) -> Option[DataA]:
+    dx, dy = deltas(direction)
     new_x = x + dx
     new_y = y + dy
     row = board.get(new_x)
     board[x][y] = 'X'
     if row is None:
-        return None
+        return Option.none()
     c = row.get(new_y)
     if c is None:
-        return None
+        return Option.none()
     if c == '#':
-        return x, y, rotate(direction)
-    return new_x, new_y, direction
+        return Option.some((x, y, rotate(direction)))
+    return Option.some((new_x, new_y, direction))
 
-def move_b(board: QList[QList[str]], x: int, y: int, direction: Literal['U', 'L', 'D', 'R'], acc: int, prev_x, prev_y) -> Tuple[int, int, str, int]:
-    deltas = {'^': (-1, 0), 'V': (1, 0), '<': (0, -1), '>': (0, 1)}
-    dx, dy = deltas[direction]
-    if rotate(direction) == board[x][y] and board.get(x + dx, QList()).get(y + dy, '') != '':
-        # found cycle
-        print('\033[91mCycle', x, y, '\033[0m')
-        acc += 1
-    if prev_x == x and prev_y == y:
-        print(direction)
-        if rotate(rotate(direction)) == board[x][y]:
-            # found cycle
-            print('\033[91mCycle', x, y, '\033[0m')
-            acc += 1
-    board[x][y] = direction
+def move_b(board: Board, x: int, y: int, direction: str, obstacles: Set[Tuple[int, int]]) -> Option[DataB]:
+    board[x][y].set(direction)
+    dx, dy = deltas(direction)
     new_x = x + dx
     new_y = y + dy
-    row = board.get(new_x)
-    if row is None:
-        return -1, -1, '', acc
-    c = row.get(new_y)
-    if c is None:
-        return -1, -1, '', acc
-    if c == '#':
-        new_x = x - dx
-        new_y = y - dy
-        while board.get(new_x, QList()).get(new_y) is not None:
-            c = board[new_x][new_y]
-            if c == '#':
-                break
-            board[new_x][new_y] = direction
-            new_x -= dx
-            new_y -= dy
-        return x, y, rotate(direction), acc
-    return new_x, new_y, direction, acc
+    next_cell: Cell = board.get(new_x, QList()).get(new_y)
 
+    # out of the maze
+    if next_cell is None:
+        return Option.none()
+
+    # obstacle up front
+    if '#' in next_cell:
+        # __fill_between_obstacles(board, x, y, dx, dy, direction)
+        return Option.some((x, y, rotate(direction), obstacles))
+
+    # path that allows for rectangular loops
+    # if rotate(direction) in next_cell and board.get(new_x + dx, QList()).get(new_y + dy, '##') != '##':
+    #     obstacles.add((new_x + dx, new_y + dy))
+
+    # path that allows for line loops
+    # if rotate(rotate(direction)) in next_cell:
+    #     new_dx, new_dy = deltas(rotate(direction))
+    #     if '#' in board.get(new_x + dx, QList()).get(new_y + dy, ''):
+    #         if '#' not in board.get(new_x + new_dx, QList()).get(new_y + new_dy, '#'):
+    #             obstacles.add((new_x + new_dx, new_y + new_dy))
+    return Option.some((new_x, new_y, direction, obstacles))
+
+
+
+def __fill_between_obstacles(board: QList[QList[str]], x: int, y: int, dx: int, dy: int, direction: str):
+    new_x = x - dx
+    new_y = y - dy
+    # go backward until an obstacle or the end of the maze is found
+    while board.get(new_x, QList()).get(new_y) is not None:
+        c = board[new_x][new_y]
+        if '#' in c:
+            break
+        if direction in c:
+            pass
+        elif c.startswith('.'):
+            board[new_x][new_y] = f'{direction}. '
+        else:
+            board[new_x][new_y] = f'{c[0]}{direction} '
+        new_x -= dx
+        new_y -= dy
+
+def __can_line_loop_be_created(board: QList[QList[str]], x: int, y: int, direction: str, obstacles: Set[Tuple[int, int]]) -> bool:
+    #....
+    #...V  (x, y)
+    #...^  (x + dx, y + dy)
+    #...#  (x + 2 * dx, y + 2 * dy) next_char
+    dx, dy = deltas(direction)
+    new_x = x + 2 * dx
+    new_y = y + 2 * dy
+    next_char = board.get(new_x, QList()).get(new_y, '')
+    if next_char == '':
+        return False
+    if next_char == '#':
+        new_direction = rotate(direction)
+        new_dx, new_dy = deltas(new_direction)
+        if board.get(x + dx + new_dx, QList()).get(y + dy + new_dy, '#') != '#':
+            obstacles.add((x + dx + new_dx, y + dy + new_dy))
+            return True
+    return False
 
 def sol_a() -> int:
     board = Lazy(read_lines('./input.txt')).map(QList).collect()
     pos = find_starting_position(board)
     assert pos is not None, 'no data in input file'
     x, y = pos
-    res = (x, y, '^')
-    while res:
-        x, y, direction = res
+    res = Option.some((x, y, '^'))
+    while res.is_some():
+        x, y, direction = res.unwrap()
         res = move(board, x, y, direction)
     return board.flatten().filter(lambda s: s == 'X').collect().len()
 
@@ -83,19 +149,33 @@ def sol_a() -> int:
 def sol_b() -> int:
     board = Lazy(read_lines('./test.txt')).map(QList).collect()
     pos = find_starting_position(board)
+    board: Board = board.map(lambda row:
+        row.map(lambda c:
+            Cell('') if c == '.' else Cell(c)
+        ).collect()
+    ).collect()
     assert pos is not None, 'no data in input file'
     x, y = pos
-    prev = 0
-    res = (x, y, '^', 0)
-    while res[0] != -1:
-        x, y, direction, acc = res
-        if acc != prev:
-            prev = acc
-            print_board(board)
-            input()
-        res = move_b(board, x, y, direction, acc, -1, -1)
-    return acc
+    res = Option.some((x, y, '^', set()))
+    print_board(board)
+    while res.is_some():
+        x, y, direction, obs = res.unwrap()
+        print(obs)
+        res = move_b(board, x, y, direction, obs)
+        print_board(board)
+        input()
 
+    # prev = 0
+    # res = (x, y, '^', set())
+    # while res[0] != -1:
+    #     x, y, direction, obs = res
+    #     if prev != len(obs):
+    #         # print_board(board)
+    #         # input()
+    #         prev = len(obs)
+    #     res = move_b(board, x, y, direction, obs)
+    # print(obs)
+    return -1
 
 def main():
     print(f'Solution a: {sol_a()}')
--- utils.py
+++ utils.py
@@ -1,11 +1,69 @@
+from collections.abc import Callable
 from typing import Generator
 from pathlib import Path
 import os
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+K = TypeVar('K')
+
 
 def read_lines(path: str | Path) -> Generator[str, None, None]:
     with open(path, 'r', encoding='utf8') as file:
         for line in file:
             yield line.strip()
+
+class Option(Generic[T]):
+    __GUARD = object()
+
+    __slots__ = ('__value',)
+
+    def __init__(self):
+        raise RuntimeError('Option can only be instantiated using Option.some(value) or Option.none()')
+
+    @classmethod
+    def some(cls, value: T) -> "Option[T]":
+        ret = super().__new__(cls)
+        ret.__value = value
+        return ret
+
+    @classmethod
+    def none(cls) -> "Option[K]":
+        ret = super().__new__(cls)
+        ret.__value = Option.__GUARD
+        return ret
+
+    def is_some(self) -> bool:
+        return self.__value != Option.__GUARD
+
+    def is_none(self) -> bool:
+        return self.__value == Option.__GUARD
+
+    def unwrap(self) -> T:
+        if self.is_none():
+            raise RuntimeError('cannot unwrap Option.none')
+        return self.__value
+
+    def unwrap_or(self, default: T) -> T:
+        if self.is_none():
+            return default
+        return self.__value
+
+    def map(self, mapper: Callable[[T], K]) -> "Option[K]":
+        if self.is_none():
+            return Option.none()
+        return Option.some(mapper(self.__value))
+
+    def __repr__(self) -> str:
+        if self.is_none():
+            return f'Option.none()'
+        return f'Option.some({self.__value!r})'
+
+    @classmethod
+    def wrap(cls, value: K | None) -> "Option[K]":
+        if value is None:
+            return Option.none()
+        return Option.some(value)
 
 
 if __name__ == '__main__':
