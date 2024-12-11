9410293c2ee546bf5594c3d8fd353b8e6c58b3c4
WitoldFracek <witek.fracek@gmail.com>
2024-12-11 10:52:09 +0100

    day 6 b

--- day6/day6.py
+++ day6/day6.py
@@ -1,6 +1,6 @@
 from qwlist import Lazy, QList
 from utils import read_lines, Option
-from typing import List, Tuple, Literal, TypeAlias, Iterable, Set, Optional
+from typing import Tuple, TypeAlias, Iterable, Set
 
 Board: TypeAlias = QList[QList["Cell"]]
 DataA: TypeAlias = Tuple[int, int, str]
@@ -9,23 +9,25 @@ DataB: TypeAlias = Tuple[int, int, str, Set[Tuple[int, int]]]
 class Cell:
     def __init__(self, *chars):
         self.chars = list(chars)
+        self.is_marked = False
 
     def __contains__(self, item: str):
         return item in self.chars
 
-    def set(self, value: str):
-        if len(self.chars) == 1 and '' in self.chars:
-            self.chars = [value]
-        elif value not in self.chars:
+    def add(self, value: str):
+        if value not in self.chars:
             self.chars.append(value)
 
-    def __repr__(self) -> str:
-        if len(self.chars) == 1 and '' in self.chars:
-            return '.   |'
-        return f'{"".join(sorted(self.chars)):<4}|'
+    def remove(self, value: str):
+        if value in self.chars:
+            self.chars.remove(value)
 
     def is_obstacle(self) -> bool:
         return '#' in self.chars
+
+    def is_visited(self) -> bool:
+        return len(self.chars) != 0
+
 
 def find_starting_position(board: Iterable[Iterable[str]]) -> Tuple[int, int] | None:
     for i, row in enumerate(board):
@@ -34,6 +36,7 @@ def find_starting_position(board: Iterable[Iterable[str]]) -> Tuple[int, int] | 
                 return i, j
     return None
 
+
 def rotate(direction: str) -> str:
     return {'^': '>', '>': 'V', 'V': '<', '<': '^'}.get(direction, direction)
 
@@ -41,17 +44,11 @@ def rotate(direction: str) -> str:
 def deltas(direction: str) -> Tuple[int, int]:
     return {'^': (-1, 0), 'V': (1, 0), '<': (0, -1), '>': (0, 1)}[direction]
 
-def neighbours(board: Board, x: int, y: int) -> Tuple[Option[Cell], Option[Cell], Option[Cell], Option[Cell]]:
-    return (
-        Option.wrap(board.get(x - 1, QList()).get(y)),
-        Option.wrap(board.get(x, QList()).get(y + 1)),
-        Option.wrap(board.get(x + 1, QList()).get(y)),
-        Option.wrap(board.get(x, QList()).get(y - 1))
-    )
 
-def print_board(board: Board):
-    print('\n'.join(board.map(lambda l: ''.join(l.map(repr)))))
-    print()
+def neighbour(board: Board, x: int, y: int, direction: str) -> Option[Cell]:
+    dx, dy = deltas(rotate(direction))
+    return Option.wrap(board.get(x + dx, QList()).get(y + dy))
+
 
 def move(board: Board, x: int, y: int, direction: str) -> Option[DataA]:
     dx, dy = deltas(direction)
@@ -69,70 +66,54 @@ def move(board: Board, x: int, y: int, direction: str) -> Option[DataA]:
     return Option.some((new_x, new_y, direction))
 
 def move_b(board: Board, x: int, y: int, direction: str, obstacles: Set[Tuple[int, int]]) -> Option[DataB]:
-    board[x][y].set(direction)
+    board[x][y].add(direction)
     dx, dy = deltas(direction)
     new_x = x + dx
     new_y = y + dy
     next_cell: Cell = board.get(new_x, QList()).get(new_y)
 
-    # out of the maze
     if next_cell is None:
         return Option.none()
 
-    # obstacle up front
-    if '#' in next_cell:
-        # __fill_between_obstacles(board, x, y, dx, dy, direction)
+    if next_cell.is_obstacle():
         return Option.some((x, y, rotate(direction), obstacles))
 
-    # path that allows for rectangular loops
-    # if rotate(direction) in next_cell and board.get(new_x + dx, QList()).get(new_y + dy, '##') != '##':
-    #     obstacles.add((new_x + dx, new_y + dy))
-
-    # path that allows for line loops
-    # if rotate(rotate(direction)) in next_cell:
-    #     new_dx, new_dy = deltas(rotate(direction))
-    #     if '#' in board.get(new_x + dx, QList()).get(new_y + dy, ''):
-    #         if '#' not in board.get(new_x + new_dx, QList()).get(new_y + new_dy, '#'):
-    #             obstacles.add((new_x + new_dx, new_y + new_dy))
+    if __can_loop(board, x, y, direction):
+        obstacles.add((new_x, new_y))
     return Option.some((new_x, new_y, direction, obstacles))
 
 
-
-def __fill_between_obstacles(board: QList[QList[str]], x: int, y: int, dx: int, dy: int, direction: str):
-    new_x = x - dx
-    new_y = y - dy
-    # go backward until an obstacle or the end of the maze is found
-    while board.get(new_x, QList()).get(new_y) is not None:
-        c = board[new_x][new_y]
-        if '#' in c:
-            break
-        if direction in c:
-            pass
-        elif c.startswith('.'):
-            board[new_x][new_y] = f'{direction}. '
-        else:
-            board[new_x][new_y] = f'{c[0]}{direction} '
-        new_x -= dx
-        new_y -= dy
-
-def __can_line_loop_be_created(board: QList[QList[str]], x: int, y: int, direction: str, obstacles: Set[Tuple[int, int]]) -> bool:
-    #....
-    #...V  (x, y)
-    #...^  (x + dx, y + dy)
-    #...#  (x + 2 * dx, y + 2 * dy) next_char
-    dx, dy = deltas(direction)
-    new_x = x + 2 * dx
-    new_y = y + 2 * dy
-    next_char = board.get(new_x, QList()).get(new_y, '')
-    if next_char == '':
+def __can_loop(board: Board, x: int, y: int, direction: str) -> bool:
+    tx, ty = deltas(direction)
+    obs_x = x + tx
+    obs_y = y + ty
+    if board[obs_x][obs_y].is_visited():
         return False
-    if next_char == '#':
-        new_direction = rotate(direction)
-        new_dx, new_dy = deltas(new_direction)
-        if board.get(x + dx + new_dx, QList()).get(y + dy + new_dy, '#') != '#':
-            obstacles.add((x + dx + new_dx, y + dy + new_dy))
-            return True
+    if rotate(direction) in board[x][y]:
+        return True
+
+    board[obs_x][obs_y].add('#')
+    steps = set()
+
+    direction = rotate(direction)
+    dx, dy = deltas(direction)
+    next_cell: Option[Cell] = Option.wrap(board.get(x + dx, QList()).get(y + dy))
+    while next_cell.is_some():
+        if next_cell.unwrap().is_obstacle():
+            if (x, y, direction) in steps:
+                board[obs_x][obs_y].remove('#')
+                return True
+            steps.add((x, y, direction))
+            direction = rotate(direction)
+            dx, dy = deltas(direction)
+            next_cell = Option.wrap(board.get(x + dx, QList()).get(y + dy))
+            continue
+        x += dx
+        y += dy
+        next_cell = Option.wrap(board.get(x + dx, QList()).get(y + dy))
+    board[obs_x][obs_y].remove('#')
     return False
+
 
 def sol_a() -> int:
     board = Lazy(read_lines('./input.txt')).map(QList).collect()
@@ -147,39 +128,24 @@ def sol_a() -> int:
 
 
 def sol_b() -> int:
-    board = Lazy(read_lines('./test.txt')).map(QList).collect()
+    board = Lazy(read_lines('./input.txt')).map(lambda s: QList(s)).collect()
     pos = find_starting_position(board)
     board: Board = board.map(lambda row:
         row.map(lambda c:
-            Cell('') if c == '.' else Cell(c)
+            Cell() if c == '.' else Cell(c)
         ).collect()
     ).collect()
     assert pos is not None, 'no data in input file'
     x, y = pos
-    res = Option.some((x, y, '^', set()))
-    print_board(board)
+    obstacles = set()
+    res = Option.some((x, y, '^', obstacles))
     while res.is_some():
-        x, y, direction, obs = res.unwrap()
-        print(obs)
-        res = move_b(board, x, y, direction, obs)
-        print_board(board)
-        input()
-
-    # prev = 0
-    # res = (x, y, '^', set())
-    # while res[0] != -1:
-    #     x, y, direction, obs = res
-    #     if prev != len(obs):
-    #         # print_board(board)
-    #         # input()
-    #         prev = len(obs)
-    #     res = move_b(board, x, y, direction, obs)
-    # print(obs)
-    return -1
+        x, y, direction, obstacles = res.unwrap()
+        res = move_b(board, x, y, direction, obstacles)
+    return len(obstacles)
 
 def main():
     print(f'Solution a: {sol_a()}')
-    # too low: 503
     print(f'Solution b: {sol_b()}')
 
 
